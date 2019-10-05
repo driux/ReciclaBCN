c68d1c5d03f2e15f1752ca2061f451ce2c514390
Raúl Higueras <raulhigueras00@gmail.com>
2019-10-05 22:21:15 +0200

    Mucha mierda

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 import os
 from app import app
-from flask import render_template
+from flask import render_template, request
+
+
 '''
 @app.route('/')
 @app.route('/index')
@@ -11,3 +13,23 @@ def index():
 @app.route('/')
 def template_test():
     return render_template('index.html', name="Luigi")
+
+
+@app.route('/punto-verde')
+def template_greenpoint():
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    print(lat, lon)
+    if lat is None or lon is None:
+        return render_template('greenpoint-redirect.html');
+    else:
+        return render_template('greenpoint.html', page_text="Hola");
+
+
+@app.route('/a-donde-va')
+def template_classifier():
+    img = request.args.get('img')
+    if img is None:
+        return render_template('classifier-img.html');
+    else:
+        return render_template('classifier.html', page_text="Hola");
