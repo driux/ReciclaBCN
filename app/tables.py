9f84864c63138385850bdaaf88575b96e5cf79b7
lena <lena@lena.asano>
2019-10-05 21:55:08 +0200

    Database listo

--- app/tables.py
+++ app/tables.py
@@ -1,6 +1,7 @@
+import os
 import sqlite3
 from sqlite3 import Error
-import create
+from create import createDatabase, createTask, insertTask
 
 def createTables(db):
     table_con = '''CREATE TABLE Containers (
@@ -24,17 +25,16 @@ def createTables(db):
     table_grp = '''CREATE TABLE GreenPoints (
                     id_grp INTEGER PRIMARY KEY,
                     nbhd_grp INTEGER,
-                    loc_fur CHAR(30),
+                    loc_grp CHAR(30),
                     lat_grp FLOAT,
                     lon_grp FLOAT,
-                    start_grp DATETIME,
-                    end_grp DATETIME,
                     FOREIGN KEY(nbhd_grp) REFERENCES Barris(id_nbhd)
                 );'''
 
     table_usr = '''CREATE TABLE Users (
                     id_usr TEXT PRIMARY KEY,
                     psw_usr CHAR(20) NOT NULL,
+                    name_usr CHAR(20) NOT NULL,
                     pts_usr INTEGER
                 );'''
 
@@ -71,35 +71,40 @@ def createBarris(db):
 
     try:
         insertTask(db, insert_nbhd)
+        db.commit()
+        print('--- Table Barris created')
     except Error as e:
         print(e)
 
 
 def createUsers(db):
     insert_usr = '''INSERT INTO Users
-                        (id_usr, psw_usr, pts_usr)
+                        (id_usr, psw_usr, name_usr, pts_usr)
                     VALUES
-                        (raul@example.com, 1234abc, 100),
-                        (jose@example.com, 1234abc, 150),
-                        (marina@example.com, 1234abc, 320),
-                        (pau@example.com, 1234abc, 250),
-                        (lena@example.com, 1234abc, 100);'''
+                        ('raul@example.com', '1234abc', 'Raul', 100),
+                        ('jose@example.com', '1234abc', 'Jose', 150),
+                        ('marina@example.com', '1234abc', 'Marina', 320),
+                        ('pau@example.com', '1234abc', 'Pau', 250),
+                        ('lena@example.com', '1234abc', 'Lena', 100);'''
 
     try:
         insertTask(db, insert_usr)
+        db.commit()
+        print('--- Table Users created')
     except Error as e:
         print(e)
 
 
 if __name__ == '__main__':
-    database = "Data/file.db"
+    database = 'data.db'
 
     db = createDatabase(os.path.abspath(database))
 
     if db is not None:
+        print('--- CREA')
         createTables(db)
         createBarris(db)
         createUsers(db)
 
     else:
-        print("No such database :(")
+        print('No such database :(')
