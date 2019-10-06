c164cfa02a631b913a8a9f75001ff57387745135
lena <lena@lena.asano>
2019-10-06 10:31:08 +0200

    src

--- app/nearest.py
+++ app/nearest.py
@@ -1,5 +1,7 @@
+import os
 import sqlite3
 from sqlite3 import Error
+from create import createDatabase
 from haversine import haversine
 from math import inf
 
@@ -7,10 +9,10 @@ def distance(my_coords, table_coords):
         return haversine(my_coords, table_coords) # en km
 
 
-def selectNearest(db, table, my_lat, my_lon):
+def selectNearest(db, my_lat, my_lon):
     my_coords = (my_lat, my_lon)
     selects = '''SELECT id_grp, lat_grp, lon_grp
-                FROM %s''' % (table)
+                FROM GreenPoints'''
 
     try:
         cur = db.cursor()
@@ -26,7 +28,25 @@ def selectNearest(db, table, my_lat, my_lon):
                 nearest = dist
                 nearest_coords = table_coords
 
-        return 'https://www.google.com/maps/place/%f,%f' % (nearest_coords[0], nearest_coords[1])
+        #return 'https://www.google.com/maps/embed/place/%f,%f' % (nearest_coords[0], nearest_coords[1])
+        return 'https://www.google.com/maps/embed/v1/place?q=%f,%f&amp' % (nearest_coords[0], nearest_coords[1])
 
     except Error as e:
         print(e)
+
+def getNearest(lat, lon):
+    return selectNearest(db, lat, lon)
+
+
+if __name__ == '__main__':
+    database = 'data.db'
+
+    db = sqlite3.connect(database)
+
+    if db is not None:
+        lat, lon = 41.385063, 2.173404
+        src = getNearest(lat, lon)
+        print(src)
+
+    else:
+        print('No such database :(')
