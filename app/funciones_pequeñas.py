1b51732f8e4c3bb686e25b1fdeae1d866aa2f031
Jose Pérez Cano <jose.perez.cano@est.fib.upc.edu>
2019-10-06 12:03:03 +0200

    more

--- app/funciones_pequeñas.py
+++
@@ -1,8 +0,0 @@
-def update_container (id, state):
-    x = "UPDATE Containers set condition_con = "+str(state)
-    return x
-
-
-def update_user_score (id, score):
-    x = "UPDATE Users set pts_usr = pts_usr + "+str(score)
-    return x
